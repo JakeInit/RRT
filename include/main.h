43dcd4c7281db5e5212dc436a643a12e6855386b
Jake <jacob.morgan@openjaus.com>
2021-04-26 09:04:24 -0400

    code clean up
    place processes in functions to shorten main()

--- include/main.h
+++ include/main.h
@@ -10,23 +10,31 @@
 
 namespace mainspace {
 
+void initialize();
 void shutdown();
 void signalHandler(int signum);
+void incrementCounter();
 
 void updateWindow(rrt::vector2f1 pt1, rrt::vector2f1 pt2);
 void placeObjectInMap(const rrt::objectNode& objectInMap);
 void placeRobotInMap(const rrt::objectNode& robotInMap);
 void placeGoalPointOnMap(rrt::vector2f1& goalPt);
+void mergeTrees();
+void findPath();
 rrt::vector2f1 convertPointToWindow(rrt::vector2f1 point);
 
 static bool running = false;
 static rrt::system::jsonParser* configReader = nullptr;
 static uint64_t windowHeight_pix;
 static uint64_t windowWidth_pix;
+static uint64_t maxNodes;
 static float windowResolution;
 static float robotHeight_m;
 static float robotWidth_m;
 static float boundarySize_m;
+static float loopTime_ms;
+static double startTime;
+static std::vector<rrt::node> pathMap;
 
 uint16_t counter;
 
--- src/apps/main.cpp
+++ src/apps/main.cpp
@@ -25,7 +25,47 @@ std::unique_ptr<rrt::rapidRandomTree> qInit = nullptr;
 std::unique_ptr<rrt::rapidRandomTree> qGoal = nullptr;
 std::unique_ptr<sf::RenderWindow> window = nullptr;
 
+void watchWindow();
+
 int main(int argc, char** argv) {
+  rrt::system::timerEvent timer;
+  initialize();
+  std::cout << "Entering the Running Loop" << std::endl;
+  while(running) {
+    if(timer.timerDone()) {
+      timer.runNonBlockingTimer_ms(std::floor(loopTime_ms));
+    } else {
+      timer.waitForTimerToFinish();
+      continue;
+    }
+
+    // Grow the trees until they connect
+    if(!qGoal->goalReached()) {
+      rrt::vector2f1 newStartTreePoint = qInit->growTreeTowardsRandom();
+      rrt::vector2f1 neighborNode = qInit->getConnectingNeighbor().location_m;
+      updateWindow(newStartTreePoint, neighborNode);
+
+      qGoal->growTreeTowardsPoint(newStartTreePoint);
+      if (!qGoal->goalReached()) {
+        rrt::vector2f1 lastPoint = qGoal->getCoordinateOfLastNode();
+        neighborNode = qGoal->getConnectingNeighbor().location_m;
+        updateWindow(lastPoint, neighborNode);
+        incrementCounter();
+      } else {
+        updateWindow(newStartTreePoint, qGoal->getConnectingNeighbor().location_m);
+      }
+    } else if (pathMap.empty()) {
+      mergeTrees();
+    } else {
+      findPath();
+    }
+  } // end run
+
+  shutdown();
+  return 0;
+}
+
+void mainspace::initialize() {
   running = true;
   signal(SIGINT, mainspace::signalHandler);
 
@@ -35,16 +75,15 @@ int main(int argc, char** argv) {
   robotHeight_m = configReader->parametersForRobot.dims.height_m;
   robotWidth_m = configReader->parametersForRobot.dims.width_m;
   boundarySize_m = configReader->parametersForSystem.boundaryWidth_m;
-  const uint64_t maxNodes = configReader->parametersForSystem.maxNodes;
+  maxNodes = configReader->parametersForSystem.maxNodes;
+
+  float loopFrequency_hz = configReader->parametersForSystem.loop_frequency_Hz;
+  loopTime_ms = 1000.0f / loopFrequency_hz;
 
   // create the window
   window = std::make_unique<sf::RenderWindow>(sf::VideoMode(windowWidth_pix, windowHeight_pix), "Dual RRT");
   windowResolution = 2*boundarySize_m/((float) windowHeight_pix);
   std::cout << "Window Resolution = " << windowResolution << std::endl;
-
-  rrt::system::timerEvent timer;
-  float loopFrequency_hz = configReader->parametersForSystem.loop_frequency_Hz;
-  float loopTime_ms = 1000.0f / loopFrequency_hz;
 
   qInit = std::make_unique<rrt::rapidRandomTree>("StartPoint", robotHeight_m, nullptr, false);
   qGoal = std::make_unique<rrt::rapidRandomTree>("GoalPoint", robotHeight_m, qInit.get(), true);
@@ -60,95 +99,15 @@ int main(int argc, char** argv) {
   placeGoalPointOnMap(goalPoint);
 
   std::cout << "Linear distance between start and end = " << rrt::rapidRandomTree::distance(qInit->getTreeStart(),
-                                                                                    qGoal->getTreeStart()) << std::endl;
+                                                                                            qGoal->getTreeStart()) << std::endl;
 
   counter = 0;
-  double startTime = rrt::system::timerEvent::getRunTime_ms();
-  std::cout << "Entering the Running Loop" << std::endl;
-  while(running) {
-    if(timer.timerDone()) {
-      timer.runNonBlockingTimer_ms(std::floor(loopTime_ms));
-    } else {
-      timer.waitForTimerToFinish();
-      continue;
-    }
-
-    // Grow the trees until they connect
-    rrt::vector2f1 newStartTreePoint = qInit->growTreeTowardsRandom();
-    rrt::vector2f1 neighborNode = qInit->getConnectingNeighbor().location_m;
-    updateWindow(newStartTreePoint, neighborNode);
-
-    qGoal->growTreeTowardsPoint(newStartTreePoint);
-    if(!qGoal->goalReached()) {
-      rrt::vector2f1 lastPoint = qGoal->getCoordinateOfLastNode();
-      neighborNode = qGoal->getConnectingNeighbor().location_m;
-      updateWindow(lastPoint, neighborNode);
-    } else {
-      updateWindow(newStartTreePoint, qGoal->getConnectingNeighbor().location_m);
-    }
-
-
-    if(qGoal->goalReached()) {
-      std::cout << "Trees are connected. Path from start to goal is possible" << std::endl;
-      double endTime = rrt::system::timerEvent::getRunTime_ms();
-      std::cout << "Time to connect trees = " << endTime - startTime << "ms" << std::endl;
-      std::cout << "Number of attempts to grow trees = " << counter << std::endl;
-      // This means the last point added to start tree connects to goal tree
-      // Need the id of the goal tree for the start tree to connect to it
-      // Then combine the trees into 1 vector
-      //  1.) add the size of the start vector to the node id in the goal tree
-      //  2.) add the size of the start vector to the node ids in the neighbors vector of each node in the goal tree
-      //  3.) add the size of the start vector to the connecting node of the goal tree
-      //  4.) add the connect node of the goal tree to the last point created in the start tree in the neighbors list
-      auto startTree = qInit->getTree();
-      auto goalTree = qGoal->getTree();
-      auto connectingGoalNode = qGoal->getConnectingNeighbor();
-
-      for(auto it : goalTree) {           // Iterate through each node in the goal tree
-        it.id += startTree.size();
-        for(auto it2 : it.neighbors) {    // Iterate through every neighbor in each node of goal tree
-          it2 += startTree.size();
-        }
-      }
-
-      connectingGoalNode.id += startTree.size();
-
-      // Add index of connecting goal node as a neighbor in the nearest node of the startTree
-      startTree.at(qInit->getIdOfLastPoint()).neighbors.emplace_back(connectingGoalNode.id);
-
-      // Create the map of possible paths for the robot in the environment
-      std::vector<rrt::node> pathMap = startTree;
-      pathMap.insert(pathMap.end(), goalTree.begin(), goalTree.end());
-      running = false;
-      std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-    } else {
-      counter++;
-    }
-
-    if(counter >= maxNodes) {
-      running = false;
-      std::cout << "No viable path found in " << maxNodes << " to grow trees." << std::endl;
-      double endTime = rrt::system::timerEvent::getRunTime_ms();
-      std::cout << "Time ran = " << endTime - startTime << "ms" << std::endl;
-      std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-    }
-  } // end run
-
-  while(window->isOpen()) {
-    // check all the window's events that were triggered since the last iteration of the loop
-    sf::Event event{};
-    while (window->pollEvent(event)) {
-      // "close requested" event: we close the window
-      if (event.type == sf::Event::Closed)
-        window->close();
-    }
-  }
-
-  shutdown();
-  return 0;
+  startTime = rrt::system::timerEvent::getRunTime_ms();
 }
 
 void mainspace::shutdown() {
+  watchWindow();
+
   if(qInit != nullptr) {
     std::cout << "Removing qInit" << std::endl;
     qInit.reset();
@@ -311,4 +270,63 @@ rrt::vector2f1 mainspace::convertPointToWindow(rrt::vector2f1 point) {
   convertedPoint.y() = point.y()/windowResolution;
   convertedPoint.y() = ((float) windowHeight_pix/2.f) - convertedPoint.y();
   return convertedPoint;
+}
+
+void mainspace::mergeTrees() {
+  std::cout << "Trees are connected. Path from start to goal is possible" << std::endl;
+  double endTime = rrt::system::timerEvent::getRunTime_ms();
+  std::cout << "Time to connect trees = " << endTime - startTime << "ms" << std::endl;
+  std::cout << "Number of attempts to grow trees = " << counter << std::endl;
+  // This means the last point added to start tree connects to goal tree
+  // Need the id of the goal tree for the start tree to connect to it
+  // Then combine the trees into 1 vector
+  //  1.) add the size of the start vector to the node id in the goal tree
+  //  2.) add the size of the start vector to the node ids in the neighbors vector of each node in the goal tree
+  //  3.) add the size of the start vector to the connecting node of the goal tree
+  //  4.) add the connect node of the goal tree to the last point created in the start tree in the neighbors list
+  auto startTree = qInit->getTree();
+  auto goalTree = qGoal->getTree();
+  auto connectingGoalNode = qGoal->getConnectingNeighbor();
+
+  for(auto it : goalTree) {           // Iterate through each node in the goal tree
+    it.id += startTree.size();
+    for(auto it2 : it.neighbors) {    // Iterate through every neighbor in each node of goal tree
+      it2 += startTree.size();
+    }
+  }
+
+  connectingGoalNode.id += startTree.size();
+
+  // Add index of connecting goal node as a neighbor in the nearest node of the startTree
+  startTree.at(qInit->getIdOfLastPoint()).neighbors.emplace_back(connectingGoalNode.id);
+
+  // Create the map of possible paths for the robot in the environment
+  pathMap = startTree;
+  pathMap.insert(pathMap.end(), goalTree.begin(), goalTree.end());
+}
+
+void mainspace::incrementCounter() {
+  counter++;
+  if(counter >= maxNodes) {
+    running = false;
+    std::cout << "No viable path found in " << maxNodes << " to grow trees." << std::endl;
+    double endTime = rrt::system::timerEvent::getRunTime_ms();
+    std::cout << "Time ran = " << endTime - startTime << "ms" << std::endl;
+  }
+}
+
+void mainspace::findPath() {
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+}
+
+void watchWindow() {
+  while(window->isOpen()) {
+    // check all the window's events that were triggered since the last iteration of the loop
+    sf::Event event{};
+    while (window->pollEvent(event)) {
+      // "close requested" event: we close the window
+      if (event.type == sf::Event::Closed)
+        window->close();
+    }
+  }
 }
